1ab8943ec058490e6c5d9d0dbb31e10500d8307e
Manuel De Leon <manueldeleon@gmail.com>
2015-06-29 17:05:36 -0600

    v1

---
+++ client/body/controls/controls.js
@@ -0,0 +1,20 @@
+Template.controls.viewmodel({
+  states: [],
+  show: false,
+  toggle: function() {
+    this.show(!this.show());
+  },
+  onRendered: function() {
+    $( "#slider" ).slider();
+  },
+  play: function(){
+
+  },
+  reverse: function(){
+
+  },
+  record: function(){
+
+  }
+
+});
---
+++ client/body/example/example.js
@@ -0,0 +1,5 @@
+Template.example.viewmodel('example', {
+  message: '',
+  selected: '',
+  colors: ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+});
