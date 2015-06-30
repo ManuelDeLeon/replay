8b2e5f3e122b4409fb66d66d933d5dd94c6f21a2
Manuel De Leon <manueldeleon@gmail.com>
2015-06-29 22:02:44 -0600

    interim

--- client/body/controls/controls.js
+++ client/body/controls/controls.js
@@ -13,8 +13,24 @@ Template.controls.viewmodel({
   reverse: function(){
 
   },
+  stopRecording: true,
+  recordingComputation: null,
   record: function(){
-
+    var that = this;
+    if (! that.stopRecording()) {
+      that.stopRecording(false);
+      var vm = ViewModel.byId("example");
+      var c = Tracker.autorun(function () {
+        that.states().push(vm.toJS());
+      });
+      that.recordingComputation(c);
+    }
+  },
+  stop: function(){
+    this.stopRecording(true);
+    if (this.recordingComputation()){
+      this.recordingComputation().stop();
+    }
   }
 
 });
